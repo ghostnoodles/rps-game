a0c7cb040e056e39b2849c7b12f6ed0d9b337e29
ghostnoodles <35480737+ghostnoodles@users.noreply.github.com>
2024-01-06 16:46:33 +1300

    added computerSelection funtion which converts the no. from getComputerChoice to either rock, paper or scissors

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Starting the Odin project Rock, Paper, and Scissors
 
 Getcomputerchoice 
 random pick r, p or s for computer option (in the form of 1,2 or 3)
-Use IF loop?
 
 playerSelection
 get player r,p or s option via prompt 
@@ -12,6 +11,9 @@ also account for if they did not type rock, paper or scissors
 
 computerSelection
 calls the Getcomputerchoice for CPU option
+1 = Rock
+2 = Paper
+3 = Scissors
 
 playRound
 IF loops? to compare player and CPU answer
--- script.js
+++ script.js
@@ -4,6 +4,22 @@
 //     return Math.floor(Math.random() * 3 + 1);
 // }
 
-let getComputerChoice = () => Math.floor(Math.random() * 3 + 1);
 
-console.log(getComputerChoice())
+
+
+let getComputerChoice = () => Math.floor(Math.random() * 3 + 1);
+let computerSelection = (num) => {
+    switch(num) {
+        case 1:
+            return 'Rock'
+            break;
+        case 2:
+            return 'Paper'
+            break;
+        case 3:
+            return 'Scissors'
+    }
+}
+
+
+console.log(computerSelection(getComputerChoice()))
