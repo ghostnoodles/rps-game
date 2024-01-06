5c3ea61e86ed4d63a00e12f2e6163f818a313874
ghostnoodles <35480737+ghostnoodles@users.noreply.github.com>
2024-01-06 15:27:26 +1300

    added function where CPU choose a number from 1 to 3 for the choice of RPS

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rps-game
 Starting the Odin project Rock, Paper, and Scissors 
+
+Getchomputerchoice 
+random pick r, p or s for computer option
---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3 + 1);
+}
+
+console.log(getComputerChoice())
